f1599eff0fbeeda49a065c94d9c5c85e7cc1ff9a
ccardinale98 <ccardinale98@gmail.com>
2021-08-20 13:00:58 -0400

    working on search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,16 @@
+var cityInputArray=[];
+var today = moment();
+var cityHistory = localStorage.getItem('City');
+console.log(cityHistory)
+
+if (cityHistory) {
+cityHistory = cityHistory.split(',')
+}
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
 }
-
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
@@ -30,4 +38,20 @@ $('#five-btn').on('click', function () {
   $('#main-todays-container').hide();
   $('#main-5-container').show();
 })
+
+$(".todays-date").text('Today is ' + today.format("MMM Do, YYYY"));
+console.log(today);
+
+$('#search-button').on('click', function () {
+  var cityInput = $('#search-input').val();
+  cityInputArray.push(cityInput);
+  localStorage.setItem('City', cityInputArray);
+  console.log(cityInputArray);
+})
+
+for (var i = 0; i < cityHistory.length; i++) {
+  var li = document.createElement('li');
+  li.textContent = cityHistory[i];
+  $('#history-list').append(li)
+}
 
