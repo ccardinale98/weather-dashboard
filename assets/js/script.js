cc4da0a3f738048a22cfa9a7393959bece0914f7
ccardinale98 <ccardinale98@gmail.com>
2021-08-20 11:35:29 -0400

    made layout for each tab

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,26 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
 }
+
+function loadDash() {
+  $('#main-dashboard-container').show();
+  $('#main-todays-container').hide();
+  $('#main-5-container').hide();
+}
+
+loadDash()
+
+$('#dash-btn').on('click', loadDash)
+
+$('#todays-btn').on('click', function () {
+  $('#main-dashboard-container').hide();
+  $('#main-todays-container').show();
+  $('#main-5-container').hide();
+})
+
+$('#five-btn').on('click', function () {
+  $('#main-dashboard-container').hide();
+  $('#main-todays-container').hide();
+  $('#main-5-container').show();
+})
+
